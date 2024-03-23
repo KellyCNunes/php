35e3ffc4407c2a94b5be80b6251ea1739961ca10
KellyCNunes <138325492+KellyCNunes@users.noreply.github.com>
2024-03-22 22:15:46 -0300

    atualização

---
+++ resposta.php
@@ -0,0 +1,17 @@
+<?php
+    
+    //$nome = $_POST['nome'];
+    //echo "Seja bem vindo $nome";
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $soma = $valor1 + $valor2;
+    $sub = $valor1 - $valor2;
+    $mult = $valor1 * $valor2;
+    $div = $valor1 / $valor2;
+    echo "<p>Soma: $soma</p>";
+    echo "<p>Subtração: $sub</p>";
+    echo "<p>Multiplicação: $mult</p>";
+    echo "<p>Divisão: $div</p>";
+
+
+
